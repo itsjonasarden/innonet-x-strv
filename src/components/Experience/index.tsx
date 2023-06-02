a973ca351064b31de3b382211a8de76cc2c0e96a
Jonas Arden <itsjonasarden@gmail.com>
2023-06-02 10:24:37 +0200

    chore: add MindAR Settings

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -1,13 +1,24 @@
+/* eslint-disable import/extensions */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { MindARThree } from 'mind-ar/dist/mindar-image-three.prod.js'
 import { useEffect, useRef } from 'react'
 
 const Experience = () => {
-  const containerRef = useRef(null)
+  const containerRef = useRef<HTMLDivElement>(null)
   useEffect(() => {
     // Initialize MindAR
     const mindAR = new MindARThree({
       container: containerRef.current,
+      imageTargetSrc: '/targets/qr-code.mind',
+      missTolerance: 10,
+      warmupTolerance: 5,
+      filterMinCF: 0.00001,
+      filterBeta: 0.1,
+      uiLoading: 'yes',
+      uiScanning: 'no',
+      uiError: 'no',
     })
   }, [])
 
